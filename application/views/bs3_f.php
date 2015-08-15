3422704d5882ebac743b20b385d12f43b7f0e68f
tsau_shrhe <boy20212@gmail.com>
2015-08-15 15:52:19 +0800

    2015/08/15 修改URL 需 index.php 問題
    1.修改config.php內容及新增.htaccess檔案
    2.修改外部文件載入方式由絕對路徑改為相對路徑

--- application/views/bs3_f.php
+++ application/views/bs3_f.php
@@ -1,6 +1,6 @@
 		<!-- jQuery -->
-		<script src="<?php echo base_url('assets/js/v1.11.3.jquery.min.js'); ?>"></script>
+		<script src="../assets/js/v1.11.3.jquery.min.js"></script>
 		<!-- Bootstrap JavaScript -->
-		<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+		<script src="../assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
